7af2cbb4dc670b7e1a2bfeb748ee8cb9a4fee8e1
XJIE6 <krav4enkoyura@yandex.ru>
2015-12-07 12:48:52 +0300

    Clicker beta ready

--- PC/ButtonClicker/src/ru/spbau/Clicker.java
+++ PC/ButtonClicker/src/ru/spbau/Clicker.java
@@ -1,13 +1,22 @@
 package ru.spbau;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Clicker implements Runnable {
 
     private long key;
+    DataInputStream in;
+    ArrayList<Command> commands;
+
 
     public Clicker() {
         Random rand = new Random();
@@ -16,13 +25,13 @@ public class Clicker implements Runnable {
         ServerSocket s;
         try {
             s = new ServerSocket(0);
-            String ip = getIp();
+            String ip = InetAddress.getLocalHost().toString().split("/")[1];
             int port = s.getLocalPort();
             System.out.println(ip);
             System.out.println(port);
             System.out.println(key);
             System.out.println("Enter '" + getCode(ip, port, key) + "' into your phone!");
-            s.accept();
+            in = new DataInputStream(s.accept().getInputStream());
             System.out.println("Accept!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,23 +45,6 @@ public class Clicker implements Runnable {
         return i;
     }
 
-    public static String getIp() throws SocketException {
-        return Collections.list(NetworkInterface.getNetworkInterfaces()).stream()
-                .flatMap(i -> Collections.list(i.getInetAddresses()).stream())
-                .filter(ip -> ip instanceof Inet4Address && ip.isSiteLocalAddress())
-                .findFirst().orElseThrow(RuntimeException::new)
-                .getHostAddress();
-
-    }
-
-    private static void toByte(long a) {
-        if (a == 0) {
-            return;
-        }
-        toByte(a / 2);
-        System.out.print(a % 2);
-    }
-
 
     private static String getCode(String ip, long port, long key) {
         long res = 0;
@@ -60,17 +52,9 @@ public class Clicker implements Runnable {
         for (String s : ip.split("\\.")) {
             res += Long.parseLong(s) << i;
             i += 8;
-
-            //System.out.println(res);
-            //toByte(res);
-            //System.out.println();
-            //toByte(Integer.parseInt(s));
-            //System.out.println();
         }
         res += port << 32;
-        //System.out.println(res);
         res += key << 48;
-        //System.out.println(res);
         String ans = "";
         while (res != 0) {
             ans = (char)((res % 94) + 33) + ans;
@@ -80,8 +64,93 @@ public class Clicker implements Runnable {
         return ans;
     }
 
+    class Command implements Runnable{
+        private  List<Integer> list;
+        Robot robot;
+        Command (List<Integer> list) {
+            this.list = list;
+            try {
+                robot = new Robot();
+            } catch (AWTException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            for (int i : list) {
+                if (i > 0) {
+                    robot.keyPress(i);
+                }
+                else {
+                    robot.keyRelease(-i);
+                }
+            }
+        }
+    }
+
     @Override
     public void run() {
+        int cmd;
+        try {
+            cmd = in.readInt();
+            if (cmd != key) {
+                return;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        while (true) {
+            try {
+                cmd = in.readInt();
+                if (cmd == 0) {
+                    break;
+                }
+                if (cmd == 1) {
+                    readCmd();
+                }
+                if (cmd == 2) {
+                    runCmd();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void runCmd() {
+        while (true) {
+            try {
+                int cmd = in.readInt();
+                if (cmd == -1) {
+                    break;
+                }
+                else  {
+                    commands.get(cmd).run();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void readCmd() {
+        try {
+            int n = in.readInt();
+            commands = new ArrayList<>();
+            for (int i = 0; i < n; ++i) {
+                int m = in.readInt();
+                List<Integer> list = new ArrayList<>();
+                for (int j = 0; j < m; ++j) {
+                    list.add(in.readInt());
+                }
+                commands.add(new Command(list));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
